896154894bbe77f8e423169f046ce194ad824a90
EdwinMeriaux <45862231+EdwinMeriaux@users.noreply.github.com>
2025-04-20 17:07:04 -0400

    Update rust_launcher.py

--- rust_launcher.py
+++ rust_launcher.py
@@ -12,27 +12,47 @@ root_dir = "rust_data"
 
 times = {}
 
-def rust_launcher(width, height, image_name, file_name,repeat,subsub,thread_count):
+def rust_launcher(width, height, image_name, file_name, repeat, subsub, thread_count):
     global times
-    time_total = 0
+    time_total = 0.0
+
     for i in range(repeat):
         args = ["cargo", "run", "--release", "--"] + [f"{width}x{height}", image_name, file_name, str(thread_count)]
-        start = time.time()
-        result = subprocess.run(args)
-        end = time.time()
-        time_total += (end-start)
+        
+        result = subprocess.run(
+            args,
+            capture_output=True,
+            text=True
+        )
+        
+        try:
+            run_time = float(result.stdout.strip())
+        except ValueError:
+            print("Error: Rust output not parseable:", result.stdout)
+            run_time = 0.0
 
-    average = time_total/repeat
-    if subsub in times.keys():
+        time_total += run_time
+
+    average = time_total / repeat
+    if subsub in times:
         times[subsub].append(average)
     else:
         times[subsub] = [average]
-    print(average)
-    print()
 
+    print(average)
+
+
+    full_data = [average]
+    csv_path = image_name
+    csv_path = csv_path[:-4]
+    csv_path += "_"
+    csv_path += str(thread_count)
+    
+    csv_path += ".csv"
+    np.savetxt(csv_path, full_data, delimiter=",")
 def dictionary_import(file_path,world_name,width,height,subsub,thread_count):
     image = file_path + "/" +  world_name
-    json_image = file_path + world_name[:-4] + ".json"
+    json_image = file_path + "/"  + world_name[:-4] + ".json"
     file_path = Path(json_image)
 
     
@@ -41,15 +61,17 @@ def dictionary_import(file_path,world_name,width,height,subsub,thread_count):
 
 thread_averages = []
 
-for thread_count in [1,2,4,8,16]:
+for thread_count in [16,8,4,2,1]:#,8,4,2,1]:
     times = {}
 
-    for subfolder in os.listdir(root_dir):
+    for subfolder in ['150']:#os.listdir(root_dir):
+        #subfolder = "50"
         subfolder_path = os.path.join(root_dir, subfolder)
 
         if os.path.isdir(subfolder_path):
             # Loop through sub-subfolders
             for subsub in os.listdir(subfolder_path):
+                
                 subsub_path = os.path.join(subfolder_path, subsub)
 
                 if os.path.isdir(subsub_path):
@@ -65,13 +87,7 @@ for thread_count in [1,2,4,8,16]:
                             #print("-" * 40)
                     
                             dictionary_import(image_folder_path,image_name,subfolder,subfolder,subsub,thread_count)
-    values = []
-    for key in times.keys():
-        average = statistics.mean(times[key])
-        values.append(average)
-    thread_averages.append(list(values))
 
-    np.save('my_matrix'+str(thread_count)+'.npy', np.matrix(values))
 
 
 print()
